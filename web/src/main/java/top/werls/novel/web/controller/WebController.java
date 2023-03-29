31e60400482320992e69a1019c715bc8f41d7631
JiaWei Lee <gsdukbh@gmail.com>
2023-03-29 22:51:26 +0800

    ui fix

--- web/src/main/java/top/werls/novel/web/controller/WebController.java
+++ web/src/main/java/top/werls/novel/web/controller/WebController.java
@@ -1,11 +1,15 @@
 package top.werls.novel.web.controller;
 
 
+import jakarta.annotation.Resource;
+import java.util.List;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import top.werls.novel.crawl.service.CrawlService;
+import top.werls.novel.crawl.vo.SearchVO;
 
 /**
  * @author Li JiaWei
@@ -16,19 +20,24 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/web")
 @Slf4j
-public class controller {
-
+public class WebController {
+  @Resource
+  private CrawlService crawlService;
 
   @RequestMapping()
   public String index(Model model) {
     log.info("  zrun  ");
     model.addAttribute("google","google");
-    return "index";
+    return "tes";
   }
 
-  @RequestMapping("/search/{novelName}")
-  public String search(@PathVariable(value = "novelName") String novelName){
-    log.info( novelName);
+  @RequestMapping("/search/{novelName}/{page:\\d+}")
+  public String search(@PathVariable(value = "novelName") String novelName,@PathVariable(value = "page") int page, Model model)
+      throws Exception {
+
+    List<SearchVO> res = crawlService.getSearch(novelName, page);
+
+    model.addAttribute("list",res);
     return "index";
   }
 }
